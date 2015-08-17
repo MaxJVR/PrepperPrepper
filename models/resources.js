c351a5cfbc4b2b8f7bea203d46cd979e076839b6
PaulRossDavidson <paulrossdavidson@gmail.com>
2015-08-17 14:09:49 -0700

    database online

--- models/resources.js
+++ models/resources.js
@@ -2,13 +2,12 @@
 module.exports = function(sequelize, DataTypes) {
   var resources = sequelize.define('resources', {
     water: DataTypes.INTEGER,
-    guns: DataTypes.INTEGER,
-    food: DataTypes.INTEGER
+    food: DataTypes.INTEGER,
+    guns: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        model.resources.belongsToMany(model.users, {through:'user_resources'});
       }
     }
   });
--- models/user.js
+++ models/user.js
@@ -2,14 +2,13 @@
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
     name: DataTypes.STRING,
-    pw: DataTypes.STRING,
+    password: DataTypes.STRING,
     email: DataTypes.STRING,
     prep_score: DataTypes.INTEGER,
     city: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
-          models.user.hasMany(models.resources, {through: "user_resources"});
         // associations can be defined here
       }
     }
