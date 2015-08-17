eb33bab2fb4633a31ed903724bc80881e40d09de
Josh Stoddard <josh@nwsigns.com>
2015-08-17 15:04:12 -0700

    changes

--- controllers/user/login.js
+++ controllers/user/login.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.send('User will log in here.');	
+//display login form
+router.get('/',function(req,res){
+    res.send('user/login');
+});
+
+//post/login
+router.post('/',function(req,res){
+  db.user.authenticate(req.body.email,req.body.password,
+    function(err,user){
+      if(err){
+        res.send(err);
+      }else if (user){
+        req.session.user = user.id;//logged in.....
+        req.flash('success', 'You are logged in')
+        res.redirect('/');
+      }else{
+        req.flash('danger','invalid username or password');
+        res.redirect('/user/login');
+      }
+  });
 });
 
 module.exports = router;
