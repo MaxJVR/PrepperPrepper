b52856cba961f5b9f344b890b9558140593c8306
Josh Stoddard <josh@nwsigns.com>
2015-08-18 10:17:08 -0700

    merge

---
+++ db.dev.js
@@ -0,0 +1,7 @@
+
+
+// Add a city
+// db.city_info.create({name: 'seattle', flood_info: 'floods', earthquake_info: 'earthquakes', marauder_info: 'arrrr.'});
+
+// clear a table
+// db.city_info.findAll().then(function(models){ for(var i=0; i<models.length; ++i){models[i].destroy()}  })
