027ab572a3e1dfe399afe19bbfadf5fec82b97c8
PaulRossDavidson <paulrossdavidson@gmail.com>
2015-08-19 16:16:22 -0700

    can pull this, changes made to controllers.

--- controllers/user/profile.js
+++ controllers/user/profile.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../../models");
 
 /* GET home page. */
+// router.get('/', function(req, res) {
+// 	res.render('user/profile', { user: req.currentUser });
+// });
+
 router.get('/', function(req, res) {
-	res.render('user/profile', {user: req.currentUser });
+  db.city_info.findAll().then(function(all_cities){
+    res.render('user/profile', {cities : all_cities, user: req.currentUser});
+  });
+});
+
+router.post("/", function(req,res){
+  db.user.update({
+    prep_score: req.body.prep_score
+  }).then(function(user){
+    res.redirect('user/profile');
+  });
 });
 
 module.exports = router;
+
