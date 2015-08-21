8a3286c1d2b7de98d474bc5a13ac8641a5cf0c68
MaxJVR <MaxJVR@gmail.com>
2015-08-20 20:44:37 -0700

    update

--- controllers/user/profile.js
+++ controllers/user/profile.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 var db = require("../../models");
 
@@ -7,19 +8,21 @@ var db = require("../../models");
 //  res.render('user/profile', { user: req.currentUser });
 // });
 
+// city = "Seattle";
+// city_state = ["Seattle":"WA"];
+// state = city_state[city];
 
-/*
-router.get('/', function(req, res) {    //fetching weather data
-    var url = 'http://api.wunderground.com/api/48693023b2ae4001/conditions/q/WA/Seattle.json';
- request(url, function(error, response, data) {
-   var parsedData = JSON.parse(data);
-   var location = parsedData.current_observation.display_location.full;
-   var temp = parsedData.current_observation.temp_f;
-   var wind = parsedData.current_observation.wind_string;
-     res.render('user/profile', {location: location, temp: temp, wind: wind});
- })
-}
-*/
+// router.get('/', function(req, res) {    //fetching weather data
+//     var url = 'http://api.wunderground.com/api/48693023b2ae4001/conditions/q/WA/Seattle.json';
+//  request(url, function(error, response, data) {
+//    var parsedData = JSON.parse(data);
+//    var location = parsedData.current_observation.display_location.full;
+//    var temp = parsedData.current_observation.temp_f;
+//    var wind = parsedData.current_observation.wind_string;
+//      res.render('user/profile', {location: location, temp: temp, wind: wind});
+//  })
+// }
+
 /*
 add to view
 <h1><%= location %></h1>
@@ -45,9 +48,17 @@ router.get('/', function(req, res) {
 
   if(req.currentUser){
     db.city.findById(req.currentUser.cityId).then(function(city){
-      res.render('user/profile', {user: req.currentUser, city : city});
+      var url = 'http://api.wunderground.com/api/48693023b2ae4001/conditions/q/WA/' + city.name + '.json';
+      request(url, function(error, response, data) {
+       var parsedData = JSON.parse(data);
+       var location = parsedData.current_observation.display_location.full;
+       var temp = parsedData.current_observation.temp_f;
+       var wind = parsedData.current_observation.wind_string;
+       var icon = parsedData.current_observation.icon_url
+        res.render('user/profile', {user: req.currentUser, city : city, location: location, temp: temp, wind: wind, icon: icon});
+      })
     });
-    }
+  }
     else{
     res.send('<h3>Why would you try and view a profile page if you are not signed in? Come on; use your head... </h3><ul><li><a href="/signup">Create Account</a></li><li><a href="/login">Log in</a></li><li><a href="/">Go Back</a></li></ul>');
   }
